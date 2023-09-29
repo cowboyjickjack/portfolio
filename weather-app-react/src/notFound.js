9a33a3d6d92112b9903a7e2c83b53a01d6e4086d
Gabriella Martinez <gabriellamartinez0819@gmail.com>
2023-09-29 10:54:21 -0500

    added conditions to search, working on error page

---
+++ weather-app-react/src/notFound.js
@@ -0,0 +1,8 @@
+export default function notFound() {
+  return (
+    <div>
+        <h2>Uh Oh! Page not found!</h2>
+        <p>Officia dolore et veniam fugiat laborum anim ullamco consequat ea est.</p>
+    </div>
+  )
+}
